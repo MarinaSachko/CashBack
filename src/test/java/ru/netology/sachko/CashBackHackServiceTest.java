4d131007f170bf6df8a107332bb7725ebc1d420d
Marina.Sachko <marina.sachko@mail.ru>
2023-07-26 22:30:24 +0300

    Create testa on Junit4

---
+++ src/test/java/ru/netology/sachko/CashBackHackServiceTest.java
@@ -0,0 +1,24 @@
+package ru.netology.sachko;
+
+import static org.junit.Assert.assertEquals;
+
+public class CashBackHackServiceTest {
+
+    @org.junit.Test
+    public void testRemain() {
+        CashBackHackService service = new CashBackHackService();
+        int amount = 900;
+        int actual = service.remain(amount);
+        int expected = 100;
+        assertEquals(actual, expected);
+    }
+
+    @org.junit.Test
+    public void testRemainBoard() {
+        CashBackHackService service = new CashBackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
+}
